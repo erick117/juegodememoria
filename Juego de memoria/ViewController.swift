98ff3280c2ae49571b99cd6c0d58a1b2aa97ec61
Erick alberto garcia marquez <erick_alberto_G@hotmail.com>
2016-08-17 20:40:27 -0500

    Código de tarea

--- Juego de memoria/ViewController.swift
+++ Juego de memoria/ViewController.swift
@@ -13,6 +13,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let numeros = 0...100
+        
+        for n in numeros {
+            //print(n)
+            
+            
+            if n >= 30 && n <= 40 {
+                print("# \(n) Viva Swift")
+            }
+            else if (n%5) == 0
+            {
+                print("# \(n) Bingo!!!")
+            }
+                
+            else if (n%2) == 0 {
+                print("# \(n) par!!!")
+            }
+            else {
+                print("# \(n) impar!!!")
+            }
+            
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
